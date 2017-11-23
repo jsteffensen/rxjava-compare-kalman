232be2d4dc094e8aad99228b8c8b17e0fb4231e3
user1211 <badabing@someemail.com>
2017-11-23 14:08:11 +0100

    adjust param

--- src/main/java/com/c2_systems/benchmark/MyBenchmark.java
+++ src/main/java/com/c2_systems/benchmark/MyBenchmark.java
@@ -34,7 +34,7 @@ import org.openjdk.jmh.infra.Blackhole;
 @State(Scope.Thread)
 public class MyBenchmark {
 
-    @Param({"1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000"})
+    @Param({"1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100", "2200", "2300", })
     public int compute;
 
     KalmanFilter kfilter;
